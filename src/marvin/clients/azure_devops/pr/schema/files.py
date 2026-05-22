d2c963c8af25a1d188a582b4530e981877cb3280
Jeffrey Clark <h0tw1r3@users.noreply.github.com>
2026-05-22 12:47:59 -0400

    fix(vcs): all AI comments linked to single file in Azure DevOps
    upstream: f20fdf6f15011863ee7c4969ab4cf6bf38f5ba9e

--- src/marvin/clients/azure_devops/pr/schema/files.py
+++ src/marvin/clients/azure_devops/pr/schema/files.py
@@ -32,6 +32,7 @@ class AzureDevOpsPRChangeSchema(BaseModel):
 
     item: AzureDevOpsPRItemSchema
     change_type: str = Field(alias="changeType")
+    change_tracking_id: int | None = Field(alias="changeTrackingId", default=None)
 
 
 class AzureDevOpsGetPRFilesQuerySchema(BaseModel):
